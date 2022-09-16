d5d7f82272cece54fa52d2f590dee52eb1051c7d
Ju4n-22 <i2226338@continental.edu.pe>
2022-09-16 05:54:36 -0500

    creacion service

--- angular/app001/src/app/clientes/listar-clientes/listar-clientes.component.ts
+++ angular/app001/src/app/clientes/listar-clientes/listar-clientes.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs';
 
+import { ClientesService } from '../clientes.service';
+
+
 @Component({
   selector: 'app-listar-clientes',
   templateUrl: './listar-clientes.component.html',
@@ -15,8 +18,9 @@ export class ListarClientesComponent implements OnInit {
   
   clientes: any[] = [];
   
-  constructor() { }
+  constructor(private service:ClientesService) { }
 
+  /*
   addCliente()
   {
 
@@ -33,15 +37,21 @@ export class ListarClientesComponent implements OnInit {
     this.clientes.splice(this.clientes.length-1,1);
 
   }
+  */
 
   ngOnInit(): void {
-
+    console.log("Hola mundo cruel")
     this.dtOptions = {
       pagingType: 'full_numbers',
       pageLength: 10
     }
 
-    this.clientes=[{tipo:"PERSONA",numero_documento:"12345678",nombres:"Luis"}]
+   /* this.clientes=[{tipo:"PERSONA",numero_documento:"12345678",nombres:"Luis"}]
+   */
+
+   this.service.getClientes().subscribe((response)=>{
+
+    });
 
     this.dtTrigger.next(null);  
   }
